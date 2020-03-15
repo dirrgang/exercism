bb8f9a5adbca344d9edde8c8004189679a377721
Dennis Irrgang <me@dennis-irrgang.com>
2020-03-15 19:57:06 +0100

    Finished exercise

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,45 @@
+import threading
+
+
 class BankAccount:
+    balance: int = 0
+    status: bool = False
+    threadLock = threading.Lock()
+
     def __init__(self):
         pass
 
     def get_balance(self):
-        pass
+        if self.status:
+            return self.balance
+        else:
+            raise ValueError(self)
 
     def open(self):
-        pass
+        if not self.status:
+            self.status = True
+        else:
+            raise ValueError(self)
 
     def deposit(self, amount):
-        pass
+        if self.status and amount > 0:
+            self.threadLock.acquire()
+            self.balance += amount
+            self.threadLock.release()
+        else:
+            raise ValueError(amount)
 
     def withdraw(self, amount):
-        pass
+        if self.status and amount > 0 and amount <= self.balance:
+            self.threadLock.acquire()
+            self.balance -= amount
+            self.threadLock.release()
+        else:
+            raise ValueError(amount)
 
     def close(self):
-        pass
+        if self.status:
+            self.status = False
+            self.balance = 0
+        else:
+            raise ValueError(self)
