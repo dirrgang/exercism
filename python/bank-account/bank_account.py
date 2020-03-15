2d5d33602b8cfdd2e260a7587a1fecd8c0ea65b2
Dennis Irrgang <me@dennis-irrgang.com>
2020-03-15 20:23:34 +0100

    Modified thread locking

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -4,7 +4,7 @@ import threading
 class BankAccount:
     balance: int = 0
     status: bool = False
-    threadLock = threading.Lock()
+    lock = threading.Lock()
 
     def __init__(self):
         pass
@@ -16,30 +16,30 @@ class BankAccount:
             raise ValueError(self)
 
     def open(self):
-        if not self.status:
-            self.status = True
-        else:
-            raise ValueError(self)
+        with self.lock:
+            if not self.status:
+                self.status = True
+            else:
+                raise ValueError(self)
 
     def deposit(self, amount):
-        if self.status and amount > 0:
-            self.threadLock.acquire()
-            self.balance += amount
-            self.threadLock.release()
-        else:
-            raise ValueError(amount)
+        with self.lock:
+            if self.status and amount > 0:
+                self.balance += amount
+            else:
+                raise ValueError(amount)
 
     def withdraw(self, amount):
-        if self.status and amount > 0 and amount <= self.balance:
-            self.threadLock.acquire()
-            self.balance -= amount
-            self.threadLock.release()
-        else:
-            raise ValueError(amount)
+        with self.lock:
+            if self.status and amount > 0 and amount <= self.balance:
+                self.balance -= amount
+            else:
+                raise ValueError(amount)
 
     def close(self):
-        if self.status:
-            self.status = False
-            self.balance = 0
-        else:
-            raise ValueError(self)
+        with self.lock:
+            if self.status:
+                self.status = False
+                self.balance = 0
+            else:
+                raise ValueError(self)
