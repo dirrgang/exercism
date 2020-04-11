429f837c6dcc76800179f4ca905257455ab77825
Dennis Irrgang <me@dennis-irrgang.com>
2020-04-11 21:19:08 +0200

    Finished exercise "Perfect Numbers"

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,24 @@
-def classify(number):
-    pass
+from typing import List
+
+
+def classify(number: int) -> str:
+    def calc_aliquot_sum(number: int) -> int:
+        divisors: List[int] = []
+
+        for num in range(number)[1:]:
+            if number % num == 0:
+                divisors.append(num)
+
+        return sum(divisors)
+
+    if number <= 0:
+        raise ValueError("Number must be greater than 0")
+
+    aliquot_sum = calc_aliquot_sum(number)
+
+    if aliquot_sum == number:
+        return "perfect"
+    elif aliquot_sum > number:
+        return "abundant"
+    else:
+        return "deficient"
