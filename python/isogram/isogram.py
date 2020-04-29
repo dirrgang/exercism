2c8924ab32db280b1d4d688082cb938196b6ed5c
Dennis Irrgang <me@dennis-irrgang.com>
2020-04-29 19:51:21 +0200

    Modified isogram and finished saddle_points

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,4 @@
 def is_isogram(string: str) -> bool:
-    normalized_string = str.lower(str.replace(str.replace(string," ", ""), "-", ""))
+    normalized_string = string.lower().replace(" ", "").replace("-", "")
 
     return len(normalized_string) == len(set(normalized_string))
--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,26 @@
-def saddle_points(matrix):
-    pass
+from typing import List
+
+
+def saddle_points(matrix: List[List[int]]) -> List[int]:
+    saddles = []
+
+    def return_row(matrix: List[List[int]], index: int) -> List[int]:
+        return matrix[index]
+
+    def return_column(matrix: List[List[int]], index: int) -> List[int]:
+        return [row[index] for row in matrix]
+
+    def verify_matrix(matrix: List[List[int]]) -> None:
+        if len(matrix) != 0:
+            for row in matrix:
+                if len(row) != len(matrix[0]):
+                    raise ValueError("Malformed Matrix!")
+
+    verify_matrix(matrix)
+
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            if matrix[x][y] == max(return_row(matrix, x)) and matrix[x][y] == min(return_column(matrix, y)):
+                saddles.append({"row": x+1, "column": y+1})
+
+    return saddles
