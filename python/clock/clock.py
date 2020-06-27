30df849cd5f5a2171b2f4899c4a65403dd165eb5
dirrgang <me@dennis-irrgang.com>
2020-06-27 10:57:26 +0200

    completed clock

---
+++ python/clock/clock.py
@@ -0,0 +1,27 @@
+from typing import Type
+from math import floor
+
+
+class Clock:
+    hour: int = 0
+    minute: int = 0
+
+    def __init__(self, hour: int, minute: int) -> None:
+        self.hour = (hour + floor((minute / 60))) % 24
+        self.minute = minute % 60
+
+    def __repr__(self) -> str:
+        return f'{self.hour:02}:{self.minute:02}'
+
+    def __eq__(self, other):
+        return self.hour % 24 == other.hour % 24 and self.minute % 60 == other.minute % 60
+
+    def __add__(self, minutes: int):
+        self.hour = (self.hour + floor((self.minute + minutes) / 60)) % 24
+        self.minute = (self.minute + minutes) % 60
+        return self
+
+    def __sub__(self, minutes: int):
+        self.hour = (self.hour + floor((self.minute - minutes) / 60)) % 24
+        self.minute = (self.minute - minutes) % 60
+        return self
