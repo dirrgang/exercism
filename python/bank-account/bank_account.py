4bfb439cef1a2c12cbba686bf8554fda1e12b14d
dirrgang <me@dennis-irrgang.com>
2020-07-03 08:15:30 +0200

    Added typing to bank_account.py

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -4,39 +4,39 @@ import threading
 class BankAccount:
     balance: int = 0
     status: bool = False
-    lock = threading.Lock()
+    lock: threading.Lock = threading.Lock()
 
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
-    def get_balance(self):
+    def get_balance(self) -> int:
         if self.status:
             return self.balance
         else:
             raise ValueError(self)
 
-    def open(self):
+    def open(self) -> None:
         with self.lock:
             if not self.status:
                 self.status = True
             else:
                 raise ValueError(self)
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> None:
         with self.lock:
             if self.status and amount > 0:
                 self.balance += amount
             else:
                 raise ValueError(amount)
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> None:
         with self.lock:
             if self.status and amount > 0 and amount <= self.balance:
                 self.balance -= amount
             else:
                 raise ValueError(amount)
 
-    def close(self):
+    def close(self) -> None:
         with self.lock:
             if self.status:
                 self.status = False
