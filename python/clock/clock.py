5114bd786c041ec54c0c42bdef31e623ee6ff48a
dirrgang <me@dennis-irrgang.com>
2020-07-03 08:10:57 +0200

    Added typing annotations to clock

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,4 +1,4 @@
-from typing import Type
+from __future__ import annotations
 from math import floor
 
 
@@ -13,15 +13,15 @@ class Clock:
     def __repr__(self) -> str:
         return f'{self.hour:02}:{self.minute:02}'
 
-    def __eq__(self, other):
+    def __eq__(self, other: Clock) -> bool:
         return self.hour % 24 == other.hour % 24 and self.minute % 60 == other.minute % 60
 
-    def __add__(self, minutes: int):
+    def __add__(self, minutes: int) -> Clock:
         self.hour = (self.hour + floor((self.minute + minutes) / 60)) % 24
         self.minute = (self.minute + minutes) % 60
         return self
 
-    def __sub__(self, minutes: int):
+    def __sub__(self, minutes: int) -> Clock:
         self.hour = (self.hour + floor((self.minute - minutes) / 60)) % 24
         self.minute = (self.minute - minutes) % 60
         return self
