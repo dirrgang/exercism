535bb5e2ba735f4b222f98e0fbc9c94eab638755
dirrgang <me@dennis-irrgang.com>
2020-07-05 21:48:25 +0200

    Completed scrabble_score.py exercise

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,11 @@
+def score(word: str) -> int:
+    letter_values: dict[str, int] = {}
+    letter_values.update(dict.fromkeys(["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"], 1))
+    letter_values.update(dict.fromkeys(["D", "G"], 2))
+    letter_values.update(dict.fromkeys(["B", "C", "M", "P"], 3))
+    letter_values.update(dict.fromkeys(["F", "H", "V", "W", "Y"], 4))
+    letter_values.update(dict.fromkeys(["K"], 5))
+    letter_values.update(dict.fromkeys(["J", "X"], 8))
+    letter_values.update(dict.fromkeys(["Q", "Z"], 10))
+
+    return sum([letter_values.get(letter, 0) for letter in str.upper(word)])
