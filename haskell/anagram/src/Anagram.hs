50b7aacac8b76ab70a596ca48d759b4f8bc1d5a5
dirrgang <me@dennis-irrgang.com>
2020-12-21 19:13:02 +0100

    Completed Anagram

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,4 +1,13 @@
 module Anagram (anagramsFor) where
 
+import qualified Data.MultiSet as MultiSet
+import qualified Data.Text as T
+
+normalizeString :: String -> String
+normalizeString xs = T.unpack $ T.toLower $ T.pack xs
+
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor xs yss =
+  [ ys | ys <- yss, MultiSet.fromList (normalizeString ys) == MultiSet.fromList (normalizeString xs)
+                      && normalizeString xs /= normalizeString ys
+  ]
