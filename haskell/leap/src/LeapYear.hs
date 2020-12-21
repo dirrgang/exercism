392bb679330b0d591cfd8710d366113c2ccd30c8
dirrgang <me@dennis-irrgang.com>
2020-12-21 21:36:30 +0100

    Completed Space Age

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,7 +2,7 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | year `mod` 400 == 0 = True
-  | year `mod` 100 == 0 = False
-  | year `mod` 4 == 0 = True
-  | otherwise = False
+  | year `rem` 4 /= 0 = False
+  | year `rem` 100 /= 0 = True
+  | year `rem` 400 /= 0 = False
+  | otherwise = True
