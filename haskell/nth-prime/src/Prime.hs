615804c4dcf2b02c7c918f0a1bac3cd4108f7d4e
dirrgang <me@dennis-irrgang.com>
2021-01-04 17:36:23 +0100

    Completed Prime

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,14 +1,18 @@
 module Prime (nth) where
 
-import Data.List (\\)
+primeTest :: Int -> Bool
+primeTest 1 = False
+primeTest 2 = True
+primeTest n = not $ or [n `mod` x == 0 | x <- [2 .. n - 1]]
 
-sieveOfEratosthenes :: Int -> [Int]
-sieveOfEratosthenes n = sieve [2 .. n] 
+nth :: Int -> Maybe Integer
+nth 0 = Nothing
+nth n = Just (toInteger (nth' 2 0))
   where
-      sieve (x : xs) = x : sieve (xs \\ [x , x + x .. n])
-      sieve [] = []
-  
-
--- nth :: Int -> Maybe Integer
-nth :: Int -> [Int]
-nth n = result where
+    nth' :: Int -> Int -> Int
+    nth' x acc
+      | n == (acc + 1) && isPrime = x
+      | isPrime = nth' (x + 1) (acc + 1)
+      | otherwise = nth' (x + 1) acc
+      where
+        isPrime = primeTest x
