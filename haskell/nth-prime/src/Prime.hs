a8f653d9f959b8925bfead1aba3d5fc88bd63fff
dirrgang <me@dennis-irrgang.com>
2021-01-04 19:55:46 +0100

    Slight modification to nth

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -3,11 +3,11 @@ module Prime (nth) where
 primeTest :: Int -> Bool
 primeTest 1 = False
 primeTest 2 = True
-primeTest n = not $ or [n `mod` x == 0 | x <- [2 .. n - 1]]
+primeTest n = not $ or [n `mod` x == 0 | x <- [2 .. (n `div` 2)]]
 
 nth :: Int -> Maybe Integer
 nth 0 = Nothing
-nth n = Just (toInteger (nth' 2 0))
+nth n = Just $ toInteger $ nth' 2 0
   where
     nth' :: Int -> Int -> Int
     nth' x acc
@@ -15,4 +15,5 @@ nth n = Just (toInteger (nth' 2 0))
       | isPrime = nth' (x + 1) (acc + 1)
       | otherwise = nth' (x + 1) acc
       where
+        isPrime :: Bool
         isPrime = primeTest x
