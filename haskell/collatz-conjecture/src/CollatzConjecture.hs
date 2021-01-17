6ac5ca59848e02b78a26439070fa8f1bcdbe5658
dirrgang <me@dennis-irrgang.com>
2021-01-17 22:28:48 +0100

    Completed collatz

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,11 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n = helper n 0
+  where
+    helper :: Integer -> Integer -> Maybe Integer
+    helper n acc
+      | n <= 0 = Nothing
+      | n == 1 = Just acc
+      | even n = helper (n `div` 2) (acc + 1)
+      | otherwise = helper (3 * n + 1) (acc + 1)
