e79a9bebb98502be2f80d445aa642dcd9a89b40b
dirrgang <me@dennis-irrgang.com>
2021-02-19 18:40:10 +0100

    Submitted first version of Nucleotide Count

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,23 @@
-module DNA (nucleotideCounts, Nucleotide(..)) where
+module DNA (nucleotideCounts, Nucleotide (..)) where
 
-import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Maybe
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+trans :: Char -> Maybe Nucleotide
+trans 'G' = Just G
+trans 'C' = Just C
+trans 'T' = Just T
+trans 'A' = Just A
+trans _ = Nothing
+
+nucleotideCounts :: String -> Either String (M.Map Nucleotide Int)
+nucleotideCounts xs
+  | all (`elem` "ACGT") xs && isJust nucleotides = Right (nucleotideCounts' (fromJust nucleotides) (M.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]))
+  | otherwise = Left []
+  where
+    nucleotides = traverse trans xs
+    nucleotideCounts' :: [Nucleotide] -> M.Map Nucleotide Int -> M.Map Nucleotide Int
+    nucleotideCounts' [] acc = acc
+    nucleotideCounts' (x : xs) acc = nucleotideCounts' xs (M.adjust (1 +) x acc)
