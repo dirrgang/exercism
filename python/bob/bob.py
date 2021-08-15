66960122880f2674828ea058b2a49cefa1724859
Dennis <d@irrgang.dev>
2021-08-15 20:10:22 +0000

    Completed Bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,13 @@
-def response(hey_bob):
-    pass
+def response(hey_bob: str):
+    hey_bob = hey_bob.strip()
+    if hey_bob == "":
+        return "Fine. Be that way!"
+    elif hey_bob.isupper():
+        if hey_bob[-1] == "?":
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Whoa, chill out!"
+    elif hey_bob[-1] == "?":
+        return "Sure."
+    else:
+        return "Whatever."
